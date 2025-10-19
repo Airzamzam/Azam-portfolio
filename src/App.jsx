d572dacb0d416bea60ec771fbf21a4b6a82c5be4
Airzamzam <53651904+Airzamzam@users.noreply.github.com>
2025-10-19 17:09:38 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Hero from "./components/Hero";
+import HeroSection from "./components/HeroSection";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
@@ -7,7 +7,7 @@ import Contact from "./components/Contact";
 export default function App() {
   return (
     <div className="bg-gray-900 text-white font-sans min-h-screen">
-      <Hero />
+      <HeroSection />
       {/* <About />
       <Projects />
       <Contact /> */}
